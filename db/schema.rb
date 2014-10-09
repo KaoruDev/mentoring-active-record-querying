aca864f9bf2c99baa7d8c156f0da8f93baf38240
Kaoru Kohashigawa <kaoru@spredfast.com>
2014-10-09 00:21:39 -0500

    Create Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009051934) do
+ActiveRecord::Schema.define(version: 20141009052100) do
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "username"
